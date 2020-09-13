689aa3e9895739a2ef69d21d3ec618ca49674e7d
sogafish <k.sogafish@gmail.com>
2020-09-14 00:08:33 +0900

    :package: Add react-hook-form

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -1,9 +1,23 @@
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from './styles.module.css'; 
 
 export default function Home() {
+  const { handleSubmit, register } = useForm();
+
+  const onSubmit = useCallback((values) => {
+    console.table(values)
+  }, []);
+
   return (
     <div className={styles.container}>
-      aaa
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="url"
+          ref={register()}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   )
 }
